294698d416c857446bcbfb40d937d9ca2d48d359
beraer <s29315@pjwstk.edu.pl>
2025-04-13 00:21:11 +0200

    DTO class was added

---
+++ src/main/java/myproject/s29315tpo06/dto/FakeDataDTO.java
@@ -0,0 +1,42 @@
+package myproject.s29315tpo06.dto;
+
+import java.util.Map;
+
+public class FakeDataDTO {
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private Map<String, String> additionalFields;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Map<String, String> getAdditionalFields() {
+        return additionalFields;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAdditionalFields(Map<String, String> additionalFields) {
+        this.additionalFields = additionalFields;
+    }
+}
