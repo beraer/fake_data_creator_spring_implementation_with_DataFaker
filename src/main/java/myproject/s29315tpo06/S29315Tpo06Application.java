c9b92b14355de1298cb90787636d445fd9d8212b
beraer <s29315@pjwstk.edu.pl>
2025-04-13 00:08:59 +0200

    Initial commit of Spring Boot project'
    :

---
+++ src/main/java/myproject/s29315tpo06/S29315Tpo06Application.java
@@ -0,0 +1,13 @@
+package myproject.s29315tpo06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S29315Tpo06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S29315Tpo06Application.class, args);
+    }
+
+}
---
+++ src/test/java/myproject/s29315tpo06/S29315Tpo06ApplicationTests.java
@@ -0,0 +1,13 @@
+package myproject.s29315tpo06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S29315Tpo06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
